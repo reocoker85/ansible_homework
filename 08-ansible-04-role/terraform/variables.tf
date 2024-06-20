18b671b319a9f07bbf8c953cbd4c6cc1cb1d9964
vagrant <vagrant@server>
2024-06-20 13:16:23 +0000

    playbook

---
+++ 08-ansible-04-role/terraform/variables.tf
@@ -0,0 +1,93 @@
+###cloud vars
+variable "token" {
+  type        = string
+  sensitive   = true
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "cloud_id" {
+  type        = string
+  sensitive   = true
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  sensitive   = true
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network&subnet name"
+}
+
+
+###vm-count vars
+variable "family" {
+  type        = string
+  default     = "fedora-37"
+  description = "OS"
+}
+
+
+
+###for_each
+variable "each_vm" {
+  type = list(object({  vm_name     = string,
+                        hostname    = string 
+                        platform    = string, 
+                        cores       = number, 
+                        memory      = number, 
+                        core_fr     = number,
+                        disk_size   = number, 
+                        preemptible = bool, 
+                        nat         = bool }))
+  default = [
+    {
+      vm_name     = "clickhouse"
+      hostname    = "clickhouse"
+      platform    = "standard-v1"
+      cores       = 4
+      memory      = 4
+      core_fr     = 20
+      disk_size   = 10 
+      preemptible = true
+      nat         = true
+    },
+    {
+      vm_name     = "vector"
+      hostname    = "vector"
+      platform    = "standard-v1"
+      cores       = 2
+      memory      = 2
+      core_fr     = 5
+      disk_size   = 10
+      preemptible = true
+      nat         = true
+    },
+    {
+      vm_name     = "lighthouse"
+      hostname    = "lighthouse"
+      platform    = "standard-v1"
+      cores       = 2
+      memory      = 2
+      core_fr     = 5
+      disk_size   = 10
+      preemptible = true
+      nat         = true
+    }
+  ]
+}
